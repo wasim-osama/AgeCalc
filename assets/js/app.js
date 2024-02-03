248c7535c2c94014e8a3d30369b34319d9477918
Wasim Akram <contact.wasimosama@gmail.com>
2024-02-03 21:09:22 +0600

    Age-Alpha-001

---
+++ assets/js/app.js
@@ -0,0 +1,11 @@
+flatpickr("#dob", {
+    altInput: true,
+    altFormat: "F j, Y",
+    disableMobile: true
+});
+flatpickr("#today", {
+    altInput: true,
+    altFormat: "F j, Y",
+    defaultDate: new Date(),
+    disableMobile: true
+});
