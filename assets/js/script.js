5bcbff5971347be78c8a0df0b265b5e462ee12c7
Wasim Akram <contact.wasimosama@gmail.com>
2024-02-09 21:51:00 +0600

    main

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,159 +1,218 @@
-// Get DOM elements
-let calcBtn = document.getElementById("calcBtn"),
-    recalBtn = document.getElementById("recalBtn"),
-    dobElem = document.getElementById("dob"),
-    todayElem = document.getElementById("today"),
+let ageCalculator = document.getElementById('ageCalculator'),
+    bmiCalculator = document.getElementById('bmiCalculator'),
+    ageCalcTab = document.getElementById('ageCalcTab'),
+    ageOverlay = document.getElementById('ageOverlay'),
+    bmiCalcTab = document.getElementById('bmiCalcTab'),
+    bmiOverlay = document.getElementById('bmiOverlay'),
+    calcBtn = document.getElementById("calcBtn"),
+    resetBtn = document.getElementById("recalBtn"),
+    bmiCalcBtn = document.getElementById("bmiCalcBtn"),
+    bmiRecalBtn = document.getElementById("bmiRecalBtn"),
     ageForm = document.getElementById("ageForm"),
+    bmiForm = document.getElementById("bmiForm"),
     ageResult = document.getElementById("ageResult"),
+    bmiResult = document.getElementById("bmiResult"),
+    dobElem = document.getElementById('dob'),
+    todayElem = document.getElementById('today'),
     yearValue = document.getElementById("yearValue"),
     monthValue = document.getElementById("monthValue"),
     dayValue = document.getElementById("dayValue"),
-    nextBirthdayDays = document.getElementById("nextBirthdayDays"),
-    nextBirthdayHours = document.getElementById("nextBirthdayHours"),
-    nextBirthdayDayName = document.getElementById("nextBirthdayDayName");
+    nextBirthdayMonthValue = document.getElementById("nextBirthdayMonth"),
+    nextBirthdayDaysValue = document.getElementById("nextBirthdayDays"),
+    nextBirthdayDayNameValue = document.getElementById("nextBirthdayDayName"),
+    weightElem = document.getElementById('weight'),
+    heightFeetElem = document.getElementById('feet'),
+    heightInchesElem = document.getElementById('inches'),
+    bmiValue = document.getElementById('bmiValue'),
+    bmiRangeVal = document.getElementById('bmiRange');
 
-// Initialize the date picker using flatpickr library
-function initDatePicker() {
-    flatpickr("#dob", {
+    
+    function initDatePicker() {
+    flatpickr('#dob', {
         altInput: true,
         altFormat: "F j, Y",
-        disableMobile: true
+        dateFormat: "Y-m-d",
     });
-
-    flatpickr("#today", {
+    flatpickr('#today', {
         altInput: true,
         altFormat: "F j, Y",
-        defaultDate: new Date(),
-        disableMobile: true
+        dateFormat: "Y-m-d",
+        defaultDate: "today"
     });
 }
 
-// Validate date of birth input
-function dobDateValidation(dob, today) {
+function dateValidate(dob, today) {
+    if (dob > today) {
+        Swal.fire({
+            title: "Are you sure?",
+            text: "The current date cannot precede the date of birth. Please ensure that the provided date of birth is accurate and corresponds to a date in the past.",
+            icon: "warning",
+            showCancelButton: false,
+            confirmButtonColor: "#3085d6",
+            confirmButtonText: "Okay!"
+        });
+        return false;
+    }
     if (dob === "") {
         Swal.fire({
-            title: "Missing Date of Birth",
-            text: "Please provide a valid date of birth. The date of birth field cannot be left empty. Ensure that you enter a valid date representing your birthdate before proceeding. If you encounter any difficulties, double-check the format and try again.",
+            title: "Are you sure?",
+            text: "Date of birth must be provided.",
             icon: "warning",
             showCancelButton: false,
             confirmButtonColor: "#3085d6",
             confirmButtonText: "Okay!"
-        })
-        return false;
-    } else if (dob > today) {
-        Swal.fire({
-            title: "Invalid Date of Birth",
-            text: "The provided date of birth must be earlier than the current date. Please ensure that the date of birth you entered is accurate and represents a date in the past. If you continue to experience issues, double-check the format and try again.",
-            icon: "warning",
-            showCancelButton: false,
-            confirmButtonColor: "#3085d6",
-            confirmButtonText: "Okay!"
-        })
+        });
         return false;
     } else {
         return true;
     }
 }
 
-// Calculate age based on the provided date of birth and current date
+function vanishAgeCalc(){
+    ageCalculator.style.display = 'none';
+    bmiCalculator.style.display = 'block';
+    bmiOverlay.style.width = "100%";
+    ageOverlay.style.width = "0";
+    console.log('hosse');
+}
+function vanishBmiCalc(){
+    ageCalculator.style.display = 'block';
+    bmiCalculator.style.display = 'none';
+    bmiOverlay.style.width = "0";
+    ageOverlay.style.width = "100%";
+    console.log('hosse');
+}
 function calculateAge(dob, today) {
-    // Convert date of birth and current date strings to Date objects
+    let dateOfBirth = new Date(dob),
+        CurrentDate = new Date(today);
+
+    let year = CurrentDate.getFullYear() - dateOfBirth.getFullYear();
+    let month = CurrentDate.getMonth() - dateOfBirth.getMonth();
+    let day = CurrentDate.getDate() - dateOfBirth.getDate();
+
+    if (month < 0 || (month === 0 && day < 0)) {
+        year--;
+        month += 12;
+    }
+    if (day < 0) {
+        const lastMonthDays = new Date(CurrentDate.getFullYear(), CurrentDate.getMonth(), 0).getDate();
+        day += lastMonthDays;
+        month--;
+
+    }
+    return {year, day, month};
+}
+function calculateNextYearBirthdate(dob,today){
     let dateOfBirth = new Date(dob);
     let currentDate = new Date(today);
 
-    // Calculate the difference in years, months, and days
-    let years = currentDate.getFullYear() - dateOfBirth.getFullYear();
-    let months = currentDate.getMonth() - dateOfBirth.getMonth();
-    let days = currentDate.getDate() - dateOfBirth.getDate();
 
-    // Adjust years and months if the birth month or day hasn't occurred yet
-    if (months < 0 || (months === 0 && days < 0)) {
-        years--;    // Subtract 1 year if the birth month or day hasn't occurred yet
-        months += 12;   // Adjust the month to be positive
+
+    let thisYearBirthday = new Date( currentDate.getFullYear(),dateOfBirth.getMonth(),dateOfBirth.getDate());
+
+    let nextYearBirthday = new Date(currentDate.getFullYear() + 1,dateOfBirth.getMonth(),dateOfBirth.getDate());
+
+
+
+
+    if (thisYearBirthday < currentDate){
+        thisYearBirthday = nextYearBirthday;
     }
 
-    // Adjust days if the birthday hasn't occurred yet this month
-    if (days < 0) {
-        // Calculate the number of days in the last month
-        const lastMonthDays = new Date(currentDate.getFullYear(), currentDate.getMonth(), 0).getDate();
 
-        // Adjust the days and subtract 1 month
-        days += lastMonthDays;
-        months--;
-    }
+    let timeDiff = thisYearBirthday - currentDate;
+    let totalDaysLeft= Math.floor(timeDiff / (1000 * 60 * 60 * 24));
+    let monthLeft= Math.floor(timeDiff / (1000 * 60 * 60 * 24 * 30 ));
+    let dayLeft = Math.floor(totalDaysLeft - (monthLeft * 30));
 
-    // Return an object containing the calculated years, months, and days
-    return { years, months, days };
+
+    let dayNameArr = ['sunday','monday','tuesday','wednesday','thursday','friday','saturday'];
+    let nextBirthdayDay = Math.floor(thisYearBirthday.getDay());
+    let nextBirthdayDayName = dayNameArr[nextBirthdayDay];
+
+
+
+
+    return {dayLeft, monthLeft, nextBirthdayDayName};
 }
 
-// Calculate days, hours, and day name until the next birthday
-function calculateNextBirthDay(dob, today) {
-    let dateOfBirth = new Date(dob);
-    let currentDate = new Date(today);
-
-    let thisYearBirthDay = new Date((new Date()).getFullYear(), dateOfBirth.getMonth(), dateOfBirth.getDate());
-    console.log(thisYearBirthDay);
-    let nextBirthDay = thisYearBirthDay;
-    let nextYearBirthDay = new Date((new Date()).getFullYear() + 1, dateOfBirth.getMonth(), dateOfBirth.getDate());
-    if (currentDate > nextBirthDay) {
-        nextBirthDay = nextYearBirthDay
-    }
-
-    let weekNames = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
-    let nextBirthdayWeekName = weekNames[nextBirthDay.getDay()];
-    const timeDifference = nextBirthDay - currentDate;
-    const leftDays = Math.floor(timeDifference / (1000 * 60 * 60 * 24));
-    const leftHours = Math.floor((timeDifference % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
-
-    return { dayName: nextBirthdayWeekName, leftDays, leftHours }
-}
-
-// Initialize the Age Calculator
-function initAgeCalculator() {
+function showAcResult() {
     let dob = dobElem.value;
     let today = todayElem.value;
 
-    // Validate date of birth
-    if (!dobDateValidation(dob, today)) {
+    let age = calculateAge(dob, today);
+    let nextBirthday = calculateNextYearBirthdate(dob,today);
+    if (!dateValidate(dob, today)) {
         return;
     }
-
-    // Calculate age and next birthday
-    let Age = calculateAge(dob, today);
-    let NextBirthDay  = calculateNextBirthDay(dob, today);
-
-    // Show the result section and hide the form
     ageForm.style.display = 'none';
     ageResult.style.display = 'block';
 
-    // Display the calculated values
-    yearValue.innerHTML = Age.years;
-    monthValue.innerHTML = Age.months;
-    dayValue.innerHTML = Age.days;
-    nextBirthdayDays.innerHTML = NextBirthDay.leftDays;
-    nextBirthdayHours.innerHTML = NextBirthDay.leftHours;
-    nextBirthdayDayName.innerHTML = NextBirthDay.dayName;
+    yearValue.innerHTML = age.year;
+    monthValue.innerHTML = age.month;
+    dayValue.innerHTML = age.day;
+    nextBirthdayMonthValue.innerHTML = nextBirthday.monthLeft;
+    nextBirthdayDaysValue.innerHTML = nextBirthday.dayLeft;
+    nextBirthdayDayNameValue.innerHTML = nextBirthday.nextBirthdayDayName;
 }
 
-// Reset the Age Calculator to show the form
-function resetAgeCalculator() {
+function bmiCalc(weight, feetHeight, inchesHeight){
+       let bodyWeight = parseFloat(weight);
+       let bodyHeightFtToMet = parseFloat(parseFloat(feetHeight) * 0.3048);
+       let bodyHeightInToMet = parseFloat(parseFloat(inchesHeight) *  0.0254);
+       let heightMeter = parseFloat(bodyHeightFtToMet + bodyHeightInToMet);
+       let bmi = parseFloat(bodyWeight / (heightMeter ** 2));
+       let bmiRounded = Math.round(bmi * 10) / 10;
+       let bmiRange = "";
+       if (bmiRounded < 18.5){
+           bmiRange = "Underweight";
+       }else if (bmiRounded <= 18.5 || bmiRounded < 25.0){
+           bmiRange = "Healthy";
+       }
+       else if (bmiRounded <= 25.0 || bmiRounded < 30){
+           bmiRange = "Overweight";
+       }
+       else if (bmiRounded > 30){
+           bmiRange = "Obese";
+       }
+    console.log(bmiRange);
+       return {bmiRounded, bmiRange};
+}
+function showBmiResult() {
+        let weight = weightElem.value;
+        let feetHeight = heightFeetElem.value;
+        let inchesHeight = heightInchesElem.value;
+        let bmiRes = bmiCalc(weight, feetHeight, inchesHeight);
+        bmiForm.style.display = "none";
+        bmiResult.style.display = "Block";
+        bmiValue.innerHTML = bmiRes.bmiRounded;
+        bmiRangeVal.innerHTML = bmiRes.bmiRange;
+
+}
+function resetAll() {
     ageForm.style.display = 'block';
     ageResult.style.display = 'none';
 
-    // Clear the displayed values
     yearValue.innerHTML = '';
     monthValue.innerHTML = '';
     dayValue.innerHTML = '';
-    nextBirthdayDays.innerHTML = '';
-    nextBirthdayHours.innerHTML = '';
-    nextBirthdayDayName.innerHTML = '';
+    nextBirthdayMonthValue.innerHTML = '';
+    nextBirthdayDaysValue.innerHTML = '';
+    nextBirthdayDayNameValue.innerHTML = '';
+}
+function resetBmi() {
+    bmiForm.style.display = "block";
+    bmiResult.style.display = "none";
+    bmiValue.innerHTML = '';
+    bmiRangeVal.innerHTML = '';
 }
 
-// Attach event listeners to Calculate and Recalculate buttons
-calcBtn.addEventListener("click", initAgeCalculator);
-recalBtn.addEventListener('click', resetAgeCalculator);
+ageCalcTab.addEventListener('click',vanishBmiCalc);
+bmiCalcTab.addEventListener('click',vanishAgeCalc);
+calcBtn.addEventListener('click', showAcResult);
+resetBtn.addEventListener('click', resetAll);
+bmiCalcBtn.addEventListener('click', showBmiResult);
+bmiRecalBtn.addEventListener('click', resetBmi);
+bmiRecalBtn.addEventListener('click', resetBmi);
 
-// Execute the date picker initialization on window load
-window.onload = () =>  {
-    initDatePicker();
-}
+window.onload = (initDatePicker);
